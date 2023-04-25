1c11ed1230bc2b14bd68a85789ccb564c89f8e54
Miezhiko <Miezhiko@gmail.com>
2023-04-25 12:29:57 +0400

    send whole message with homaridae
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Kalmarity/Bot/Commands/Homaridae.hs
+++ src/Kalmarity/Bot/Commands/Homaridae.hs
@@ -6,7 +6,6 @@ import           Kalmarity.Homaridae
 
 import           Kalmarity.Bot.Config
 import           Kalmarity.Bot.Database
-import           Kalmarity.Bot.Utils
 
 import           Calamity
 
@@ -16,7 +15,7 @@ import           Calamity.Commands.Context (FullContext)
 import           Control.Monad
 import           Control.Monad.IO.Class    (MonadIO, liftIO)
 
-import           Data.Default
+import           Data.List
 import           Data.Text                 (Text, unpack)
 
 import           Optics
@@ -34,15 +33,19 @@ registerHomaridaeCommand ∷
   ) => P.Sem (DSLState FullContext r) ()
 registerHomaridaeCommand = void
     $ help (const "Homaridae command.")
-    $ commandA @'[Text] "homaridae" []
-    $ \ctx txt -> do
+    $ commandA @'[[Text]] "homaridae" ["h"]
+    $ \ctx ltxt -> do
       -- Just _gld <- pure (ctx ^. #guild)
       kafkaAddress <- P.asks @Config $ view #kafkaAddress
       let msgId  = show (ctx ^. #message % to (getID :: Message -> Snowflake Message))
           chanId = show (ctx ^. #channel % to (getID :: Channel -> Snowflake Channel))
           authId = show (ctx ^. #user % to    (getID :: User -> Snowflake User))
           genKey = chanId ++ "|" ++ authId ++ "|" ++ msgId
-      liftIO $ produceKafkaMessage kafkaAddress genKey (unpack txt)
+          inTxt  = intercalate " " $ map unpack ltxt
+      liftIO $ produceKafkaMessage kafkaAddress genKey inTxt
+      -- no woofies
+      {-
       tell_ @Embed ctx $ def
           & #title ?~ "Homaridae"
           & #description ?~ "woof"
+      -}
