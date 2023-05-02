0c4f1c649959d1bff0a2821635e1732ef34fed78
Miezhiko <Miezhiko@gmail.com>
2023-05-02 16:32:47 +0400

    move kafka message processing to new module, handle channel messages
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Kalmarity/Homaridae/Consumer.hs
+++ src/Kalmarity/Homaridae/Consumer.hs
@@ -5,22 +5,18 @@
 module Kalmarity.Homaridae.Consumer
   ( consumerProps
   , consumerSub
-  , processKafkaMessages
   , runKafkaConsumer
   ) where
 
 import           Calamity
 
 import           Kalmarity.Homaridae.Kafka
-import           Kalmarity.Homaridae.SnowFlake
+import           Kalmarity.Homaridae.Processor
 
+import           Control.Concurrent            (threadDelay)
 import           Control.Concurrent.Async
 import           Control.Exception             (SomeException, bracket, catch)
-   
-import           Control.Monad
 
-import qualified Data.ByteString.Char8         as BSC
-import           Data.Maybe                    (fromMaybe)
 import           Data.Text
 
 import           Kafka.Consumer
@@ -37,31 +33,6 @@ consumerSub ∷ Subscription
 consumerSub = topics [consumerTopic]
            <> offsetReset Earliest
 
-processKafkaMessages ∷ KafkaConsumer
-                    -> ((Snowflake Channel, Text) -> IO (Maybe ()))
-                    -> ((Snowflake Message, Text) -> IO (Maybe ()))
-                    -> IO ()
-processKafkaMessages kafkaConsumer msgIO replyIO = do
-  result <- pollMessage kafkaConsumer (Timeout 2000)
-  case result of
-    Left err ->
-      case err of
-        KafkaResponseError rerr ->
-          case rerr of
-            RdKafkaRespErrTimedOut -> pure ()
-            _                      -> putStrLn $ "Polling response error: " ++ show err
-        _ -> putStrLn $ "Error polling message: " ++ show err
-    Right msg -> do
-      let myKey = BSC.unpack $ fromMaybe BSC.empty (crKey msg)
-          myVal = BSC.unpack $ fromMaybe BSC.empty (crValue msg)
-      case parseSnowflakesTuple myKey of
-        Left err                         -> print err
-        Right (_channelId, _user, messageId) -> do
-          void $ replyIO (messageId, (pack myVal))
-      _ <- commitAllOffsets OffsetCommit kafkaConsumer
-      putStrLn "Offset committed"
-  processKafkaMessages kafkaConsumer msgIO replyIO
-
 -- run two workers
 runConsumerSubscription ∷ KafkaConsumer
                        -> ((Snowflake Channel, Text) -> IO (Maybe ()))
@@ -71,6 +42,11 @@ runConsumerSubscription kafkaConsumer msgIO replyIO = do
   workerTask1 <- async $
     catch (processKafkaMessages kafkaConsumer msgIO replyIO)
       (\(e :: SomeException) -> putStrLn $ "Consumer loop exception: " ++ show e)
+
+  -- run second worker second afer
+  -- each worker poll rate is 2s
+  threadDelay 1000000
+
   workerTask2 <- async $
     catch (processKafkaMessages kafkaConsumer msgIO replyIO)
       (\(e :: SomeException) -> putStrLn $ "Consumer loop exception: " ++ show e)
---
+++ src/Kalmarity/Homaridae/Processor.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE
+    OverloadedStrings
+  #-}
+
+module Kalmarity.Homaridae.Processor
+  ( processKafkaMessages
+  ) where
+
+import           Calamity
+
+import           Kalmarity.Homaridae.SnowFlake
+   
+import           Control.Monad
+
+import qualified Data.ByteString.Char8         as BSC
+import           Data.Maybe                    (fromMaybe)
+import           Data.Text
+
+import           Kafka.Consumer
+
+processSingleMessage ∷ String
+                    -> String
+                    -> ((Snowflake Channel, Text) -> IO (Maybe ()))
+                    -> ((Snowflake Message, Text) -> IO (Maybe ()))
+                    -> IO ()
+processSingleMessage myKey myVal msgIO replyIO =
+  case parseSnowflakesTuple myKey of
+    Left err                             -> print err
+    Right (channelId, userId, messageId) ->
+      case messageId of
+        Snowflake 0 -> case userId of
+              Snowflake 0 -> void $ msgIO (channelId, (pack myVal))
+              u -> let withMention = "<@" ++ show u ++ "> " ++ myVal
+                   in void $ msgIO (channelId, (pack withMention))
+        m -> void $ replyIO (m, (pack myVal))
+
+processKafkaMessages ∷ KafkaConsumer
+                    -> ((Snowflake Channel, Text) -> IO (Maybe ()))
+                    -> ((Snowflake Message, Text) -> IO (Maybe ()))
+                    -> IO ()
+processKafkaMessages kafkaConsumer msgIO replyIO = do
+  result <- pollMessage kafkaConsumer (Timeout 2000)
+  case result of
+    Left err ->
+      case err of
+        KafkaResponseError rerr ->
+          case rerr of
+            RdKafkaRespErrTimedOut -> pure ()
+            _                      -> putStrLn $ "Polling response error: " ++ show err
+        _ -> putStrLn $ "Error polling message: " ++ show err
+    Right msg -> do
+      let myKey = BSC.unpack $ fromMaybe BSC.empty (crKey msg)
+          myVal = BSC.unpack $ fromMaybe BSC.empty (crValue msg)
+      processSingleMessage myKey myVal msgIO replyIO
+      _ <- commitAllOffsets OffsetCommit kafkaConsumer
+      putStrLn "Offset committed"
+  processKafkaMessages kafkaConsumer msgIO replyIO
