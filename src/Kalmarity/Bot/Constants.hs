6c66938b952133a9427dd38b97d0aa0b5674d938
Miezhiko <Miezhiko@gmail.com>
2023-05-15 11:29:36 +0400

    limit kalmarity more
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

---
+++ src/Kalmarity/Bot/Constants.hs
@@ -0,0 +1,12 @@
+module Kalmarity.Bot.Constants where
+
+import           Calamity
+
+ownUserId ∷ Snowflake User
+ownUserId = Snowflake 1096396952117198868
+
+ownGuildId ∷ Snowflake Guild
+ownGuildId = Snowflake 611822838831251466
+
+modRoleId ∷ Snowflake Role
+modRoleId = Snowflake 907158486016729088
--- src/Kalmarity/Bot/Handlers/Messages.hs
+++ src/Kalmarity/Bot/Handlers/Messages.hs
@@ -3,22 +3,41 @@ module Kalmarity.Bot.Handlers.Messages where
 import           Kalmarity.Homaridae
 
 import           Kalmarity.Bot.Config
+import           Kalmarity.Bot.Constants
 
 import           Calamity
 
 import           Optics
 
 import           Control.Monad
-import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           Control.Monad.IO.Class  (MonadIO, liftIO)
 
-import           Data.Text              (replace)
+import qualified Data.Text               as T
+import qualified Data.Vector.Unboxing    as VU
 
-import qualified Polysemy               as P
-import qualified Polysemy.Fail          as P
-import qualified Polysemy.Reader        as P
+import qualified Polysemy                as P
+import qualified Polysemy.Fail           as P
+import qualified Polysemy.Reader         as P
 
 --containsRussian ∷ [Char] -> Bool
 --containsRussian = any (\x -> 1040 <= ord x && ord x <=1103) ∘ filter isLetter
+
+aiResponse ∷ (Is k1 A_Getter, Is k2 A_Getter, Is k3 A_Getter,
+              Is k4 A_Getter, Show a1, Show a2, JoinKinds k5 A_Getter k3,
+              LabelOptic "id" k1 s s a1 a1,
+              LabelOptic
+                "content" k2 s s T.Text T.Text,
+              LabelOptic "channelID" k4 s s a2 a2,
+              LabelOptic "author" k5 s s MessageAuthor MessageAuthor,
+              MonadIO m) =>
+  T.Text -> s -> m ()
+aiResponse kafkaAddress kmsg =
+  let msgId  = show $ kmsg ^. #id
+      chanId = show $ kmsg ^. #channelID
+      authId = show $ show $ kmsg ^. #author % to (getID :: MessageAuthor -> Snowflake User)
+      genKey = chanId ++ "|" ++ authId ++ "|" ++ msgId
+      inTxt  = T.replace "<@1096396952117198868>" "" (kmsg ^. #content)
+  in liftIO $ produceKafkaMessage kafkaAddress genKey inTxt
 
 registerMessagesHandler ∷
   ( BotC r
@@ -28,15 +47,16 @@ registerMessagesHandler ∷
     ] r
   , MonadIO (P.Sem r)  -- Add MonadIO constraint
   ) => P.Sem r () -- (Message, Maybe User, Maybe Member)
-registerMessagesHandler = void $ react @'MessageCreateEvt $ \(kmsg, _mbU, _mbM) ->
+registerMessagesHandler = void $ react @'MessageCreateEvt $ \(kmsg, _mbU, mbM) ->
   let kmentions   = kmsg ^. #mentions
       kmentionIds = map (\u -> (getID :: User -> Snowflake User) u) kmentions
-  -- TODO: get bot id somehow else
-  in when ((Snowflake 1096396952117198868) `elem` kmentionIds) $ do
+  in when (ownUserId `elem` kmentionIds) $ do
     kafkaAddress <- P.asks @Config $ view #kafkaAddress
-    let msgId  = show $ kmsg ^. #id
-        chanId = show $ kmsg ^. #channelID
-        authId = show $ kmsg ^. #author % to (getID :: MessageAuthor -> Snowflake User)
-        genKey = chanId ++ "|" ++ authId ++ "|" ++ msgId
-        inTxt  = replace "<@1096396952117198868>" "" (kmsg ^. #content)
-    liftIO $ produceKafkaMessage kafkaAddress genKey inTxt
+    Just gld <- pure (kmsg ^. #guildID)
+    if (ownGuildId == gld)
+      then aiResponse kafkaAddress kmsg
+      else do
+        Just msgMem <- pure mbM
+        let mRoles   = msgMem ^. #roles
+        when (modRoleId `VU.elem` mRoles) $
+          aiResponse kafkaAddress kmsg
